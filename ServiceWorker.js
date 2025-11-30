08d266796b76dd57f1d5e568ab77c4d3b7c9ad95
QuatiConde <victormanoelxzk@hotmail.com>
2025-11-29 22:41:13 -0300

    att2

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "AruanaEstudio-Petisquinho-1.0";
+const contentToCache = [
+    "Build/331c2daebf49a03641a72a899463e6dc.loader.js",
+    "Build/026168fe77b55a1a49b838afa17cb5e6.framework.js",
+    "Build/0ff0ed81ca1ab70741d3387f1b9a0368.data",
+    "Build/504ec242e5ba5e4ee2278aeb7595feaa.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
