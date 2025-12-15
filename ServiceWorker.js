3df397764fb85497859ce3ca15e885f76092d398
QuatiConde <victormanoelxzk@hotmail.com>
2025-12-14 23:31:46 -0300

    update 9

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "AruanaEstudio-Petisquinho-1.0.8";
 const contentToCache = [
-    "Build/105e682755ab58ee5895021f5dba40c9.loader.js",
+    "Build/37685b68af2035f564669a86e7969778.loader.js",
     "Build/a8e9b85afdcd722a6c2b2f21fc417856.framework.js",
-    "Build/7a454695cf02c4f9eea0724a92d1b6a0.data",
-    "Build/b22489dd42cd6fbd4b6f8c00fa71998d.wasm",
+    "Build/3b7b895457d292d779fa3cc0b2109b73.data",
+    "Build/699c3e76fb16393b3435da01c266c0d4.wasm",
     "TemplateData/style.css"
 
 ];
