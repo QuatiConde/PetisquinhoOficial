d05d68b9e033ed789dbe27985982a24f5a4fc8ed
QuatiConde <victormanoelxzk@hotmail.com>
2025-12-18 11:39:03 -0300

    atualizações minimas de botao do tutorial

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -2,7 +2,7 @@ const cacheName = "AruanaEstudio-Petisquinho-1.0.8";
 const contentToCache = [
     "Build/c42fdf266db6c9c80232f55a07a090b1.loader.js",
     "Build/a8e9b85afdcd722a6c2b2f21fc417856.framework.js",
-    "Build/cce243f071a072751075344c1d88c54d.data",
+    "Build/726f545771b55cc89be298533ea8c241.data",
     "Build/f0cd895d0b311ef51237b6aa6e06bcbd.wasm",
     "TemplateData/style.css"
 
