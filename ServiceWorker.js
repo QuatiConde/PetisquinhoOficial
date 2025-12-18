7f60fc3c832e4c481edd1a00647b44c75b2fd98f
QuatiConde <victormanoelxzk@hotmail.com>
2025-12-18 11:19:55 -0300

    Ajuste do Menu

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -2,7 +2,7 @@ const cacheName = "AruanaEstudio-Petisquinho-1.0.8";
 const contentToCache = [
     "Build/c42fdf266db6c9c80232f55a07a090b1.loader.js",
     "Build/a8e9b85afdcd722a6c2b2f21fc417856.framework.js",
-    "Build/ed3fc10d34b7f095542d769f15fb1a77.data",
+    "Build/e7d64953456cbef4768be87ba55309f9.data",
     "Build/f0cd895d0b311ef51237b6aa6e06bcbd.wasm",
     "TemplateData/style.css"
 
